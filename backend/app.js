46b6bb34ebcd7c32a25184e969ad37ac2e7fd84c
Adam <adam.tremblett7@gmail.com>
2020-03-20 15:09:40 -0400

    Finished mongodb section

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://adam:yOH36auoHkefwEzI@cluster0-jv3bi.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to database!')
+  })
+  .catch(() => {
+    console.log('Connection failed!')
+  });
 
 app.use(bodyParser.json());
 // not required, just an example that body parser can parse different types
@@ -18,30 +29,35 @@ app.use((req, res, next) => {
   next();
 });
 
+//password yOH36auoHkefwEzI
+
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'asdfasdfasldkgjasdflkg',
-      title: 'First server-side post',
-      content: 'this is coming from the server'
-    },
-    {
-      id: 'adfskljgdfahkjlaboirr',
-      title: 'Second server-side post',
-      content: 'this is coming from the server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+app.get("/api/posts", (req, res, next) => {
+  Post.find()
+  .then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(req.params.id);
+    res.status(200).json({ message: "Post deleted!" });
   });
 });
 
