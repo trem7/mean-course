6302f7e30978128d9b722a442f664926deef080e
Adam <adam.tremblett7@gmail.com>
2020-03-24 13:14:45 -0400

    SPA Authentication section complete

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -24,6 +25,42 @@ router.post("/signup", (req, res, next) => {
             error: err
           });
         });
+    });
+});
+
+// login route
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result){
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      // create new token based on input data
+
+      // third object is to configure
+      const token = jwt.sign(
+        // email and user id from database
+        { email: user.email, userId: user._id },
+        // provide a secret used to hash
+        'secret_this_should_be_longer',
+        // configuration
+        { expiresIn: "1h" }
+        );
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
     });
 });
 
