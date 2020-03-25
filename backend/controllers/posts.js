31ddb0cc10d0946aa7a43ef11a04aa0f972fe653
Adam <adam.tremblett7@gmail.com>
2020-03-25 14:51:27 -0400

    Update bug fixed

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -40,7 +40,7 @@ exports.updatePost =  (req, res, next) => {
   });
   // updateOne takes arguments of post id and creator id
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-    if(result.nModified > 0) {
+    if(result.n > 0) {
       res.status(200).json({message: "Update successful!"});
     } else {
       res.status(401).json({message: "User not authorized to edit"});
