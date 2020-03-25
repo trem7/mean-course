ba132ac38a1665f5601bcfee13123b4324ccaa07
Adam <adam.tremblett7@gmail.com>
2020-03-25 10:24:06 -0400

    Added user id to posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -26,7 +26,8 @@ export class PostsService {
                 title: post.title,
                 content: post.content,
                 id: post._id,
-                imagePath: post.imagePath
+                imagePath: post.imagePath,
+                creator: post.creator
               };
             }),
             maxPosts: postData.maxPosts
@@ -34,6 +35,7 @@ export class PostsService {
         })
       )
     .subscribe(transformedPostData => {
+      console.log(transformedPostData);
       this.posts = transformedPostData.posts;
       this.postsUpdated.next({
         posts: [...this.posts],
